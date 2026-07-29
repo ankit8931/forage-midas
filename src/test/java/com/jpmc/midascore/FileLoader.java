031d23f9debe10b1eefbfc58e30e78806472bc12
Ankit Kushwaha <ankitkushwaha8931@gmail.com>
2026-07-29 18:26:23 +0530

    Completed Task 1 and Task 2 setup

--- src/test/java/com/jpmc/midascore/FileLoader.java
+++ src/test/java/com/jpmc/midascore/FileLoader.java
@@ -1,19 +1,31 @@
 package com.jpmc.midascore;
 
 import org.springframework.stereotype.Component;
-import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
+import org.apache.commons.io.IOUtils;
 
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class FileLoader {
     public String[] loadStrings(String path) {
-        try {
-            InputStream inputStream = this.getClass().getResourceAsStream(path);
-            String fileText = IOUtils.toString(inputStream, "UTF-8");
-            return fileText.split(System.lineSeparator());
+        // try-with-resources: resource leak se bachata hai aur stream ko automatically close karta hai
+        try (InputStream inputStream = this.getClass().getResourceAsStream(path)) {
+
+            // Null check: agar file path galat ho ya file na mile
+            if (inputStream == null) {
+                return new String[0];
+            }
+
+            // Safe UTF-8 conversion
+            String fileText = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+
+            // \\r?\\n Regex: Windows (\r\n) aur Linux/Mac (\n) dono line breaks ko handle karega
+            return fileText.split("\\r?\\n");
+
         } catch (Exception e) {
-            return null;
+            // Error aane par crash hone ki jagah empty array return karega
+            return new String[0];
         }
     }
 }
